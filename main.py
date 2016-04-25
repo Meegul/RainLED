4f8f1daddc205a6d19b8d8733abde88145fee992
Michael Bilstein <Micha3lbilstein@gmail.com>
2016-04-25 01:41:25 -0400

    Initial Commit
    This prints a 3x3 array, with West Lafayette in the center, of
    rain/cloud data.

---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+import json
+
+def printStuff(kek):
+	for i in range(9):
+		print kek[i]['station']['coord']['lat'],
+		if (i % 3 == 2):
+			print
+	for i in range(9):
+		print kek[i]['station']['coord']['lon'],
+		if (i % 3 == 2):
+			print
+	print 'kek'
+	return
+
+url = 'http://api.openweathermap.org/data/2.5/station/find'
+APIKey = ''
+payload = {'lat': '40.42','lon': '-86.91', 'cnt': 9, 'APPID': APIKey}
+stations = []
+r = requests.get(url, params=payload)
+
+#add stations json data to an array and print weather info
+for i in range(9):
+	stations.append(r.json()[i])
+	#try: print stations[i]['last']['clouds'][0]['condition'], "cloud"
+    #	except KeyError: pass
+	#try: print stations[i]['last']['rain']['1h'], "rain"
+    #	except KeyError: pass
+
+#printStuff(stations)
+
+#sort stations by longitute
+for i in range(8):
+	for n in range(8):
+		if (stations[n]['station']['coord']['lon'] < stations[n+1]['station']['coord']['lon']):
+			temp = stations[n+1]
+			stations[n+1] = stations[n]
+			stations[n] = temp
+
+#printStuff(stations)
+
+#sort stations by latitude, but only 3 at a time, so as to not change which row they're in
+# 12 51 31 21 56 78 90 13 78 becomes
+# 51 31 12 78 56 21 90 78 13
+for i in range(3):
+	for n in range(2):
+		if (stations[i*3 + n]['station']['coord']['lat'] < stations[i*3 + n + 1]['station']['coord']['lat']):
+			temp = stations[i*3 + n +1]
+			stations[i*3 + n +1] = stations[i*3 + n]
+			stations[i*3 + n] = temp
+
+#printStuff(stations)
+
+#flips 0 and 2 spots in a row
+# 51 31 12 78 56 21 90 78 13 becomes
+# 12 31 51 21 56 78 13 78 90
+# I'm aware that I should've just fixed the previous sort
+# But you do it. I was having problems. This just works
+for i in range(3):
+	temp = stations[i*3]
+	stations[i*3] = stations[i*3 + 2]
+	stations[i*3 + 2] = temp
+
+#printStuff(stations)
+
+for i in range(9):
+	try: print stations[i]['last']['clouds'][0]['condition'],# "cloud",
+    	except KeyError: pass
+	try: print stations[i]['last']['rain']['1h'],# "rain",
+    	except KeyError: pass
+    	
+    	if (i % 3 == 2):
+    		print
