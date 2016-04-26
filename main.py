d82da0278fb52a0f2650438d1f79e8620c007daa
Michael Bilstein <Micha3lbilstein@gmail.com>
2016-04-25 20:35:29 -0400

    Redid the main logic
    Now it actually finds a 3x3 array, 100km out from the center

--- main.py
+++ main.py
@@ -1,86 +1,71 @@
 import requests
 import json
+import math
 import time
-
-#for debugging purposes
-def printStuff(kek):
-	for i in range(9):
-		print kek[i]['station']['coord']['lat'],
-		if (i % 3 == 2):
-			print
-	for i in range(9):
-		print kek[i]['station']['coord']['lon'],
-		if (i % 3 == 2):
-			print
-	print 'kek'
-	return
-
 url = 'http://api.openweathermap.org/data/2.5/station/find'
 APIKey = ''
-payload = {'lat': '40.42','lon': '-86.91', 'cnt': 9, 'APPID': APIKey}
-stations = []
+latitude = '40.42'
+longitude = '-86.91'
+payload = {'lat': latitude,'lon': longitude, 'cnt': 100, 'APPID': APIKey}
+
 while (1):
-        log = open('logs.txt' , 'a')
+	log = open('logs.txt' , 'a')
+	log.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) + "\n")
+	print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
 	r = requests.get(url, params=payload)
 
-	#add stations json data to an array if it doesn't have 9 already
-	if (len(stations) < 9):
-		for i in range(9):
-			stations.append(r.json()[i])
-	#else, replace first 9 with the new data
-	else:
-		for i in range(9):
-			stations[i] = r.json()[i]
+	counter = 0
+	rainStations = []
 
-	#printStuff(stations)
+	#finds stations with rain data
+	for i in range(50):
+		try:
+			#print r.json()[i]['last']['rain']['1h'], counter
+			x = r.json()[i]['last']['rain']['1h']
+			#ensures no duplicates
+			if not(r.json()[i] in rainStations):
+				rainStations.append(r.json()[i])
+				counter += 1
+		except KeyError: pass
 
-	#sort stations by longitute
-	for i in range(8):
-		for n in range(8):
-			if (stations[n]['station']['coord']['lon'] < stations[n+1]['station']['coord']['lon']):
-				temp = stations[n+1]
-				stations[n+1] = stations[n]
-				stations[n] = temp
+	#Array: 
+	#0 1 2
+	#3 4 5
+	#6 7 8
+	array = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+	smallestDistances = [100,100,100,100,100,100,100,100,100]
 
-	#printStuff(stations)
+	#Chooses stations with distances closest to: 
+	#(-100km, +100km) (+0km, +100km) (+100km, +100km)
+	# (-100km, +0km)   (+0km, +0km)   (+100km, +0km)
+	#(-100km, -100km) (+0km, -100km) (+100km, -100km)
+	for q in rainStations:
+		lat = q['station']['coord']['lat']
+		lon = q['station']['coord']['lon']	
+		for i in range(3):
+			for n in range(3):
+				index = i + n*3
+				x = lon - float(longitude) - i*1 + 1
+				y = lat - float(latitude) + n*1 - 1
+				distance = math.sqrt(pow(x, 2) + pow(y, 2))
+				if (distance < smallestDistances[index]):
+					array[index] = q;
+					smallestDistances[index] = distance
 
-	#sort stations by latitude, but only 3 at a time, so as to not change which row they're in
-	# 12 51 31 21 56 78 90 13 78 becomes
-	# 51 31 12 78 56 21 90 78 13
-	for i in range(3):
-		for n in range(2):
-			if (stations[i*3 + n]['station']['coord']['lat'] < stations[i*3 + n + 1]['station']['coord']['lat']):
-				temp = stations[i*3 + n +1]
-				stations[i*3 + n +1] = stations[i*3 + n]
-				stations[i*3 + n] = temp
+	#prints selected stations' locations
+	#for i in array:
+	#	lat = i['station']['coord']['lat']
+	#	lon = i['station']['coord']['lon']
+	#	print "(" + str(lon) + "," + str(lat) + ")"# "Distance:", distance
 
-	#printStuff(stations)
-
-	#flips [0] and [2] spots in a row
-	# 51 31 12 78 56 21 90 78 13 becomes
-	# 12 31 51 21 56 78 13 78 90
-	# I'm aware that I should've just fixed the previous sort
-	# But you do it. I was having problems. This just works
-	for i in range(3):
-		temp = stations[i*3]
-		stations[i*3] = stations[i*3 + 2]
-		stations[i*3 + 2] = temp
-
-	#printStuff(stations)
-
-	log.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) + "\n")
 	for i in range(9):
-		try:
-			print stations[i]['last']['clouds'][0]['condition'],;# "cloud",
-			log.write("" + stations[i]['last']['clouds'][0]['condition']+ " ")
-	    	except KeyError: pass
-		try:
-			print stations[i]['last']['rain']['1h'],# "rain",
-			log.write("" + str(stations[i]['last']['rain']['1h']) + " ")
-	    	except KeyError: pass
+		log.write(str(array[i]['last']['rain']['1h']))
+		print array[i]['last']['rain']['1h'],
+		if (i % 3 == 2):
+			log.write('\n')
+			print
+	print
+	log.close()
+	time.sleep(15)
 
-	    	if (i % 3 == 2):
-	    		log.write('\n')
-	    		print
-        log.close()
-	time.sleep(900)
+
