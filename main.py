a10f64861ab7a6495b26f8fa53951300632b7d99
Michael Bilstein <Micha3lbilstein@gmail.com>
2016-05-07 21:00:09 -0500

    Converted to 5x5 grid, as opposed to 3x3. Offers more accurate data. Had to pull data 5x, as opposed to once in order to increase accuracy

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ url = 'http://api.openweathermap.org/data/2.5/station/find'
 APIKey = ''
 latitude = ''
 longitude = ''
-payload = {'lat': latitude,'lon': longitude, 'cnt': 100, 'APPID': APIKey}
+payload = {'lat': latitude,'lon': longitude, 'cnt': 50, 'APPID': APIKey}
 
 if (len(sys.argv) == 2 and sys.argv[1] == 'log'):
 	toLog = 1
@@ -19,63 +19,85 @@ while (1):
 		log = open('logs.txt' , 'a')
 		log.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) + "\n")
 	print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
-	r = requests.get(url, params=payload)
 
-	counter = 0
+        r = []
+	temp = requests.get(url, params=payload)
+
+        #requests data from spots:
+        #(0, 2, 4, 10, 12, 14, 02, 22, 24)
+        for y in range(3):
+            for x in range(3):
+                payload = {'lat': str(float(latitude) + x - 1),'lon': str(float(longitude) - y + 1), 'cnt': 50, 'APPID': APIKey}
+                temp = requests.get(url, params=payload)
+                for n in temp.json():
+                    if (n not in r):
+                        r.append(n)
+        counter = 0
 	rainStations = []
 
-	#finds stations with rain data
-	for i in range(50):
-		try:
-			#print r.json()[i]['last']['rain']['1h'], counter
-			x = r.json()[i]['last']['rain']['1h']
-			#ensures no duplicates
-			if not(r.json()[i] in rainStations):
-				rainStations.append(r.json()[i])
-				counter += 1
-		except KeyError: pass
+        #finds stations with rain data
+	for i in range(len(r)):
+            try:
+		x = r[i]['last']['rain']['1h']
+		#ensures no duplicates
+		if not(r[i] in rainStations):
+		    rainStations.append(r[i])
+		    counter += 1
+	    except KeyError: pass
 
-	#Array: 
-	#0 1 2
-	#3 4 5
-	#6 7 8
-	array = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-	smallestDistances = [100,100,100,100,100,100,100,100,100]
+        #Array:
+	#0  1  2  3  4
+	#5  6  7  8  9
+        #10 11 12 13 14
+	#15 16 17 18 19
+        #20 21 22 23 24
+	array = []
+        smallestDistances = []
+        for i in range(25):
+            array.append(-1)
+            smallestDistances.append(100)
 
-	#Chooses stations with distances closest to: 
-	#(-100km, +100km) (+0km, +100km) (+100km, +100km)
-	# (-100km, +0km)   (+0km, +0km)   (+100km, +0km)
-	#(-100km, -100km) (+0km, -100km) (+100km, -100km)
+	#Chooses stations with distances closest to:
+	#(-100km, +100km) (-50km, +100km) (+0km, +100km) (+50km, +100km) (+100km, +100km)
+        #(-100km, +50km)  (-50km, +50km)  (+0km, +50km)  (+50km, +50km)  (+100km, +50km)
+	#(-100km, +0km)   (-50km, +0km)   (+0km, +0km)   (+50km, +0km)   (+100km, +0km)
+        #(-100km, -50km)  (-50km, -50km)  (+0km, -50km)  (+50km, -50km)  (+100km, -50km)
+	#(-100km, -100km) (-50km, -100km) (+0km, -100km) (+50km, -100km) (+100km, -100km)
 	for q in rainStations:
 		lat = q['station']['coord']['lat']
-		lon = q['station']['coord']['lon']	
-		for i in range(3):
-			for n in range(3):
-				index = i + n*3
-				x = lon - float(longitude) - i*1 + 1
-				y = lat - float(latitude) + n*1 - 1
-				distance = math.sqrt(pow(x, 2) + pow(y, 2))
+		lon = q['station']['coord']['lon']
+		for i in range(5):
+			for n in range(5):
+                                index = n + i*5
+				x = lon - float(longitude) - n*.5 + 1
+				y = lat - float(latitude) + i*.5 - 1
+                                distance = math.sqrt(pow(x, 2) + pow(y, 2))
+                                #print "Index: " + str(index),
+                                #print "x: " + str(x),
+                                #print "y: " + str(y),
+                                #print "distance: " + str(distance)
 				if (distance < smallestDistances[index]):
 					array[index] = q;
 					smallestDistances[index] = distance
 
-	#prints selected stations' locations
-	#for i in array:
-	#	lat = i['station']['coord']['lat']
+        #prints selected stations' locations
+        #for i in array:
+        #       lat = i['station']['coord']['lat']
 	#	lon = i['station']['coord']['lon']
-	#	print "(" + str(lon) + "," + str(lat) + ")"# "Distance:", distance
+	#	print "(" + str(lat) + "," + str(lon) + ")"
 
-	for i in range(9):
-		if (toLog):
-			log.write(str(array[i]['last']['rain']['1h']))
-		print array[i]['last']['rain']['1h'],
-		if (i % 3 == 2):
-			if (toLog):
-				log.write('\n')
-			print
+
+        #outputs final results
+	for i in range(25):
+	    if (toLog):
+	        log.write(str(array[i]['last']['rain']['1h']))
+            print (str(1) if array[i]['last']['rain']['1h'] != 0 else str(0)),
+	    if (i % 5 == 4):
+	        if (toLog):
+                    log.write('\n')
+		print
 	print
 	if (toLog):
 		log.close()
-	time.sleep(900)
-
-
+	#waits 15 minutes
+        time.sleep(900)
