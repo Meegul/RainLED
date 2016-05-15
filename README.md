da2119c733acd85cebd496227003929e6eb9402a
Meegul304 <micha3lbilstein@gmail.com>
2016-05-15 16:13:36 -0500

    Added ability to change the dimension used by passing it as an argument after main.py. Might be buggy, but initial testing shows it functions

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # RainLED
-This is to be used with a Raspberry Pi in order to create a 5x5 LED matrix of rain data. Useful as a decoration.
+This is to be used with a Raspberry Pi in order to create an LED matrix of rain data. Useful as a decoration.
 It is currently a work in progress. I have done limited testing, but in theory, it should 
 work in other locations, whilst dynamically selecting the best weather station to represent each LED.
 
+Specify the desired dimension (only takes odds, and will replace evens with odds) as an argument after main.py
+Defaults to 5x5 if no dimension is specified
+Dimension must be at least 3, or the program will go to the default of 5
+
 The data is currently just outputted to the console every 15 minutes, along with a timestamp
 If 'log' is passed as an argument, this program will log data to a logs.txt file every 15 minutes.
+
+Configure your APIKey and coordinates in the specified variables in main.py
 
 # You must use your own OpenWeatherMap API Key!
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def printCoords(array):
 
 #Takes rain data and determines if an LED should turn on
 def shouldTurnOn(rain):
-    return 1 if rain != 0 else 0
+    return 1 if rain > .5 else 0
 
 #-----CONFIGURATION-VARIABLES-----#
 #Configure these to suit your needs
@@ -25,9 +25,32 @@ longitude = ''
 payload = {'lat': latitude,'lon': longitude, 'cnt': 50, 'APPID': APIKey}
 url = 'http://api.openweathermap.org/data/2.5/station/find'
 
+#Determines if too many arguments were passed
+if (len(sys.argv) > 3):
+    print "Usage: python main.py [dimension 3-9 (3 for 3x3)(defaults to 5x5)] [log (optional)]"
+    quit()
+
+#Determines what the inputted dimension is
+#If none was given, default to 5
+if (len(sys.argv) > 1):
+    try:
+        dimension = int(float(sys.argv[1]))
+    except ValueError:
+        dimension = 5
+else: dimension = 5
+
+#ensures dimension is at least 3
+if (dimension < 3):
+    dimension = 5
+
+#Ensures dimension is odd
+#This ensures there's a center
+if (dimension % 2 == 0):
+    dimension += 1
+
 #Determines whether the user specified whether the program
 #should log the output to logs.txt
-if (len(sys.argv) == 2 and sys.argv[1] == 'log'):
+if ('log' in sys.argv):
 	toLog = 1
 else: toLog = 0
 
@@ -36,13 +59,18 @@ while (1):
 	if(toLog):
 		log = open('logs.txt' , 'a')
 		log.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) + "\n")
+                print "Logging..."
 	print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
 
         r = []
 	temp = requests.get(url, params=payload)
 
         #requests data from spots:
-        #(0, 2, 4, 10, 12, 14, 02, 22, 24)
+        #X-X-X
+        #-----
+        #X-X-X
+        #-----
+        #X-X-X
         for y in range(3):
             for x in range(3):
                 payload = {'lat': str(float(latitude) + x - 1),'lon': str(float(longitude) - y + 1), 'cnt': 50, 'APPID': APIKey}
@@ -53,7 +81,7 @@ while (1):
         counter = 0
 	rainStations = []
 
-        #finds stations with rain data
+        #finds stations that contain rain data
 	for i in range(len(r)):
             try:
 		x = r[i]['last']['rain']['1h']
@@ -63,7 +91,7 @@ while (1):
 		    counter += 1
 	    except KeyError: pass
 
-        #Array:
+        #5x5 Array index example:
 	#0  1  2  3  4
 	#5  6  7  8  9
         #10 11 12 13 14
@@ -71,11 +99,11 @@ while (1):
         #20 21 22 23 24
 	array = []
         smallestDistances = []
-        for i in range(25):
+        for i in range(pow(dimension, 2)):
             array.append(-1)
             smallestDistances.append(100)
 
-        #Chooses a station closest to each location shown here:
+        #Chooses a station closest to each location shown here in a 5x5 example:
 	#(-100km, +100km) (-50km, +100km) (+0km, +100km) (+50km, +100km) (+100km, +100km)
         #(-100km, +50km)  (-50km, +50km)  (+0km, +50km)  (+50km, +50km)  (+100km, +50km)
 	#(-100km, +0km)   (-50km, +0km)   (+0km, +0km)   (+50km, +0km)   (+100km, +0km)
@@ -84,22 +112,23 @@ while (1):
 	for q in rainStations:
 		lat = q['station']['coord']['lat']
 		lon = q['station']['coord']['lon']
-		for i in range(5):
-			for n in range(5):
-                                index = n + i*5
-				x = lon - float(longitude) - n*.5 + 1
-				y = lat - float(latitude) + i*.5 - 1
+                dif = 1/float(dimension/2) #determines the distance that should be incremented in a single loop. ex: .3 for 7x7
+		for i in range(dimension):
+			for n in range(dimension):
+                                index = i*dimension + n
+				x = lon - float(longitude) - n*dif + 1
+				y = lat - float(latitude) + i*dif - 1
                                 distance = math.sqrt(pow(x, 2) + pow(y, 2))
 				if (distance < smallestDistances[index]):
 					array[index] = q;
 					smallestDistances[index] = distance
 
         #Outputs final results
-	for i in range(25):
+	for i in range(pow(dimension, 2)):
 	    if (toLog):
                 log.write(str(shouldTurnOn(float(array[i]['last']['rain']['1h']))))
             print (str(shouldTurnOn(float(array[i]['last']['rain']['1h'])))),
-	    if (i % 5 == 4):
+	    if (i % dimension == dimension-1):
 	        if (toLog):
                     log.write('\n')
 		print
